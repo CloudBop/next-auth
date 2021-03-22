9e64290835e189e509aa70aaf12f8f6ea9890e57
Cloudbop <boppincolin@gmail.com>
2021-03-22 15:53:41 +0000

    configd auth-login so redirects if user already logged in

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import AuthForm from "../components/auth/auth-form";
-
+import { getSession } from "next-auth/client";
+import { useRouter } from "next/router";
 function AuthPage() {
+  //
+  //- TODO MOVE LOGIC TO ServerSideProps
+  //
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    getSession().then(session => {
+      if (session) {
+        // redirect to profile
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+    // return () => {
+    //   cleanup
+    // }
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
   return <AuthForm />;
 }
 
