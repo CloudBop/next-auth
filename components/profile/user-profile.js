1946bb58a9dee32ead7a4caab0ce0c8ce7e2abe3
Cloudbop <boppincolin@gmail.com>
2021-03-22 15:42:47 +0000

    validate on the the server instead of component

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,26 +1,24 @@
-import { useState, useEffect } from "react";
-
-import { getSession } from "next-auth/client";
+// import { useState, useEffect } from "react";
+// import { getSession } from "next-auth/client";
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
-  const [isLoading, setIsLoading] = useState(true);
-  // const [loadedSession, setLoadedSession] = useState();
-
-  useEffect(() => {
-    //
-    getSession().then(session => {
-      if (!session) {
-        // navigate away
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-        // setLoadedSession(session);
-      }
-    });
-  }, []);
+  // const [isLoading, setIsLoading] = useState(true);
+  // // const [loadedSession, setLoadedSession] = useState();
+  // useEffect(() => {
+  //   //
+  //   getSession().then(session => {
+  //     if (!session) {
+  //       // navigate away
+  //       window.location.href = "/auth";
+  //     } else {
+  //       setIsLoading(false);
+  //       // setLoadedSession(session);
+  //     }
+  //   });
+  // }, []);
 
   if (isLoading) {
     return <p className={classes.profile}>Loading...</p>;
