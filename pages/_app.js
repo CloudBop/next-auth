68d4b83e8686b5ead2c446b316c5451e35ffdae4
Cloudbop <boppincolin@gmail.com>
2021-03-22 16:00:41 +0000

    added next-auth Provider to minimise duplicate get session requests

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Layout from "../components/layout/layout";
+import { Provider } from "next-auth/client";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  // pageProps are passed via SSR. EG /profile route sets session on  ssr
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
